03f05553f8c05b2eea7a3c7f9e960e008922c6e1
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-07-14 18:12:22 -0300

    feat: coloca anotacao de allArgsContructor no DTO para o teste

--- src/main/java/com/dh/mercadolivre/desafioquality/dto/RoomAreaDto.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/dto/RoomAreaDto.java
@@ -1,10 +1,12 @@
 package com.dh.mercadolivre.desafioquality.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class RoomAreaDto {
     private String roomName;
     private Double area;
