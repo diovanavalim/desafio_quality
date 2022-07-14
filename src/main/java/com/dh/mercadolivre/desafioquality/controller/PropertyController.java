173ba0c01195fd322cdb1cf6b6126ce6ad145fe7
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-14 15:47:48 -0300

    feat: Inclui rota que busca o valor total da propriedade

--- src/main/java/com/dh/mercadolivre/desafioquality/controller/PropertyController.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/controller/PropertyController.java
@@ -20,20 +20,31 @@ public class PropertyController {
 	@Autowired
 	PropertyService service;
 
-	@GetMapping("/totalpropertyarea/{id}")
-	public ResponseEntity<Double> getTotalPropertyArea(Long id) {
+	// buscar area total da propriedade pelo ID
+	@GetMapping("/totalPropertyArea/{id}")
+	public ResponseEntity<Double> getTotalPropertyArea(@PathVariable  Long id) {
 		Double totalPropertyArea = service.getAreaTotal(id);
 
 		return ResponseEntity.ok(totalPropertyArea);
 	}
 
-	@GetMapping("/largestroom/{id}")
+	// buscar valor total da propriedade
+	@GetMapping("/totalPropertyPrice/{id}")
+	public ResponseEntity<Double> getTotalPropertyPrice(@PathVariable Long id) {
+		Double totalPropertyPrice = service.calculateTotalPropertyPrice(id);
+
+		return ResponseEntity.ok(totalPropertyPrice);
+	}
+
+	// buscar o maior cômodo de uma propriedade pelo ID
+	@GetMapping("/largestRoom/{id}")
 	public ResponseEntity<RoomAreaDto> getLargestRoom(@PathVariable Long id){
 		RoomAreaDto largestRoom = service.getLargestRoomFromId(id);
 
 		return ResponseEntity.ok(largestRoom);
 	}
 
+    // buscar a lista de cômodos e suas áreas calculadas (retorna nome e área de cada cômodo)
 	@GetMapping("/listRooms/{id}")
 	public ResponseEntity<List<RoomAreaDto>> getListRoom(@PathVariable Long id){
 		List<RoomAreaDto> listRoom = service.getAreaRooms(id);
