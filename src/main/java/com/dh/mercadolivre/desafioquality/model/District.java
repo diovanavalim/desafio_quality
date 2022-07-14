a50479ce2c00f2bb349206904a954f493b4c896a
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-14 14:50:36 -0300

    fix: fix validations

--- src/main/java/com/dh/mercadolivre/desafioquality/model/District.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/model/District.java
@@ -10,13 +10,13 @@ import javax.validation.constraints.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class District {
-//    @NotBlank(message = "O nome do bairro não pode estar vazio.")
-//    @Size(max = 45, message = "O comprimento do nome do bairro não pode exceder 45 caracteres.")
+    @NotBlank(message = "O nome do bairro não pode estar vazio.")
+    @Size(max = 45, message = "O comprimento do nome do bairro não pode exceder 45 caracteres.")
     private String name;
 
     private String city;
 
-//    @NotNull(message = "O valor do metro quadrado não pode estar vazio.")
-//    @DecimalMax(value = "999999999999.99", message = "O valor do metro quadrado não pode ser maior que 999999999999.99.")
+    @NotNull(message = "O valor do metro quadrado não pode estar vazio.")
+    @DecimalMax(value = "999999999999.99", message = "O valor do metro quadrado não pode ser maior que 999999999999.99.")
     private double valueDistrictM2;
 }
--- src/main/java/com/dh/mercadolivre/desafioquality/model/Property.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/model/Property.java
@@ -12,15 +12,15 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Property {
-//    @NotBlank(message = "O nome da propriedade não pode estar vazio.")
-    /*@Pattern(regexp = "([A-Z]{1}[a-z]+\\\\s??)+", message = "O nome da propriedade deve começar com uma letra maiúscula.")*/
-//    @Size(max = 30, message = "O comprimento do nome da propriedade não pode exceder 30 caracteres.")
+    @NotBlank(message = "O nome da propriedade não pode estar vazio.")
+    @Pattern(regexp = "(?=^.{2,60}$)^[A-ZÀÁÂĖÈÉÊÌÍÒÓÔÕÙÚÛÇ][a-zàáâãèéêìíóôõùúç]+(?:[ ](?:das?|dos?|de|e|[A-Z][a-z]+))*$", message = "O nome da propriedade deve começar com uma letra maiúscula.")
+    @Size(max = 30, message = "O comprimento do nome da propriedade não pode exceder 30 caracteres.")
     private String propName;
 
-//    @Valid
+    @Valid
     private District propDistrict;
 
-//    @NotEmpty(message = "A lista de cômodos não pode estar vazia.")
+    @NotEmpty(message = "A lista de cômodos não pode estar vazia.")
     private List<@Valid Room> roomList;
 
     private long id;
