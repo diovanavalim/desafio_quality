5dbb00433c31ffdd704919e38a7234b48deb5463
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-15 11:02:34 -0300

    feat: add getDistrictList method

--- src/test/java/com/dh/mercadolivre/desafioquality/util/DistrictGenerators.java
+++ src/test/java/com/dh/mercadolivre/desafioquality/util/DistrictGenerators.java
@@ -2,6 +2,9 @@ package com.dh.mercadolivre.desafioquality.util;
 
 import com.dh.mercadolivre.desafioquality.model.District;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DistrictGenerators {
 
     public static District getDistrict() {
@@ -10,5 +13,40 @@ public class DistrictGenerators {
                 .city("Florianopolis")
                 .valueDistrictM2(600.00)
                 .build();
+    }
+
+    public static List<District> getDistrictList() {
+        List<District> districtList = new ArrayList<District>();
+
+        District district01 = District.builder()
+                .name("Carvoeira")
+                .city("Florianopolis")
+                .valueDistrictM2(600.00)
+                .build();
+
+        District district02 = District.builder()
+                .name("Trindade")
+                .city("Florianopolis")
+                .valueDistrictM2(900.00)
+                .build();
+
+        District district03 = District.builder()
+                .name("Saco Grande")
+                .city("Florianopolis")
+                .valueDistrictM2(600.00)
+                .build();
+
+        District district04 = District.builder()
+                .name("Itacorubi")
+                .city("Florianopolis")
+                .valueDistrictM2(900.00)
+                .build();
+
+        districtList.add(district01);
+        districtList.add(district02);
+        districtList.add(district03);
+        districtList.add(district04);
+
+        return districtList;
     }
 }
