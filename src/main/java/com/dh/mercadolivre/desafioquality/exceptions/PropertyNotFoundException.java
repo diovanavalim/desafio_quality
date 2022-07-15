9dac154dd0d4898b31987e13fe312c8bf1ef0bbc
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-15 17:34:13 -0300

    doc: Inclusão de comentários na classe PropertyNotFoundException

--- src/main/java/com/dh/mercadolivre/desafioquality/exceptions/PropertyNotFoundException.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/exceptions/PropertyNotFoundException.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Diovana Valim, Gabriela Azevedo
  * @version 0.0.1
  */
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PropertyNotFoundException extends RuntimeException {
 
     /**
      * Constructor method called whether an readFile operation for Property class has no results.
-     * @param message custom message displayed to user by the server response
+     * @param message custom message displayed to user by the server response.
      */
     public PropertyNotFoundException(String message) {
         super(message);
