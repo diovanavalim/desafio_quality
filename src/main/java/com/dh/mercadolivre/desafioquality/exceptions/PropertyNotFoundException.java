f56248492a7eab89e34c69c1a90510c88e26564f
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-15 13:53:22 -0300

    feat: add javadocs

--- src/main/java/com/dh/mercadolivre/desafioquality/exceptions/PropertyNotFoundException.java
+++ src/main/java/com/dh/mercadolivre/desafioquality/exceptions/PropertyNotFoundException.java
@@ -3,9 +3,19 @@ package com.dh.mercadolivre.desafioquality.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Class that extends of the RunTimeException error class. It represents the custom Exception PropertyNotFound.
+ * @author Diovana Valim, Gabriela Azevedo
+ * @version 0.0.1
+ */
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PropertyNotFoundException extends RuntimeException {
 
+    /**
+     * Constructor method called whether an readFile operation for Property class has no results.
+     * @param message custom message displayed to user by the server response
+     */
     public PropertyNotFoundException(String message) {
         super(message);
     }
